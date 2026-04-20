6b701f36fd3a1c0667434c536f9cbda5cc342179
oqid <sdal739@aucklanduni.ac.nz>
2026-04-20 17:50:49 +1200

    sevans here

--- include/helpers.h
+++ include/helpers.h
@@ -8,7 +8,7 @@ int FindMinIndex(float arr[], int n);
 int getIndex(float arr[], int n, float desired);
 void fast_flash_double_LED_builtin();
 void slow_flash_LED_builtin();
-void AlignWithWall(int lr_sr_mode);
+// void AlignWithWall(int lr_sr_mode);
 float angle_diff(float target, float current);
 
 #endif
