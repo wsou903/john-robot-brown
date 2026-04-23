569bfddba33170e86bb03f369427f4b807d53c85
Will Southee <wsou903@aucklanduni.ac.nz>
2026-04-23 18:23:20 +1200

    slam

--- include/slam.h
+++ include/slam.h
@@ -3,10 +3,8 @@
 
 #include "config.h"
 
-
-
-
 // this gets called continuously
 void budget_slam();
+void dump_slam_data();
 
 #endif
--- src/slam.cpp
+++ src/slam.cpp
@@ -1,13 +1,21 @@
 #include "slam.h"
 #include "helpers.h"
 #include "sensors.h"
-\
+
+// --- SLAM DATA STORAGE ---
+const int MAX_SLAM_POINTS = 500; // 500 points at 100ms = 50 seconds of recording. Adjust as needed!
+float history_X[MAX_SLAM_POINTS];
+float history_Y[MAX_SLAM_POINTS];
+// float history_Heading[MAX_SLAM_POINTS];
+int slam_point_count = 0;
 // helper functions to set sensor limits
-float getValidSR(float raw) {
+float getValidSR(float raw)
+{
     return (raw >= 4.0 && raw <= 30.0) ? raw : -1.0;
 }
 
-float getValidLR(float raw) {
+float getValidLR(float raw)
+{
     return (raw >= 10.0 && raw <= 80.0) ? raw : -1.0;
 }
 
@@ -19,7 +27,7 @@ void budget_slam()
     float us_front = getUSDistance();
     float sr_front_left = getValidSR(getLeftSR());
     float sr_front_right = getValidSR(getRightSR());
-    float lr_left = getValidLR(getLeftLR());  // Assuming Left LR is on Left side
+    float lr_left = getValidLR(getLeftLR());   // Assuming Left LR is on Left side
     float lr_right = getValidLR(getRightLR()); // Assuming Right LR is on Right side
 
     int x_count = 0;
@@ -30,14 +38,17 @@ void budget_slam()
     // 2. COORDINATE CALCULATION LAMBDA
     auto addCoordinateEstimate = [&](float distance, float mounted_angle)
     {
-        if (distance <= 0) return; // Ignore invalid readings
+        if (distance <= 0)
+            return; // Ignore invalid readings
 
         // Robot orientation + where the sensor is pointing
         float global_angle = robot_heading + mounted_angle;
 
         // Normalize angle to -PI to PI
-        while (global_angle > PI)  global_angle -= TWO_PI;
-        while (global_angle < -PI) global_angle += TWO_PI;
+        while (global_angle > PI)
+            global_angle -= TWO_PI;
+        while (global_angle < -PI)
+            global_angle += TWO_PI;
 
         float c = cos(global_angle);
         float s = sin(global_angle);
@@ -45,16 +56,20 @@ void budget_slam()
         // Calculate X (Pointing toward Front/Back walls)
         if (abs(c) > 0.707) // Within 45 degrees of X axis
         {
-            if (c > 0) sum_x += TABLE_HEIGHT - (distance * abs(c));
-            else       sum_x += (distance * abs(c));
+            if (c > 0)
+                sum_x += TABLE_HEIGHT - (distance * abs(c));
+            else
+                sum_x += (distance * abs(c));
             x_count++;
         }
 
         // Calculate Y (Pointing toward Side walls)
         if (abs(s) > 0.707) // Within 45 degrees of Y axis
         {
-            if (s > 0) sum_y += distance * abs(s);
-            else       sum_y += TABLE_WIDTH - (distance * abs(s));
+            if (s > 0)
+                sum_y += distance * abs(s);
+            else
+                sum_y += TABLE_WIDTH - (distance * abs(s));
             y_count++;
         }
     };
@@ -70,288 +85,45 @@ void budget_slam()
     addCoordinateEstimate(lr_right, -HALF_PI); // -90 degrees
 
     // 4. AVERAGE THE RESULTS
-    if (x_count > 0) robotX = sum_x / x_count;
-    if (y_count > 0) robotY = sum_y / y_count;
+    if (x_count > 0)
+        robotX = sum_x / x_count;
+    if (y_count > 0)
+        robotY = sum_y / y_count;
 
-    // 5. OUTPUT DATA
-    static unsigned long print_timer = 0;
-    if (now - print_timer > 100)
+    // 5. SAVE DATA TO ARRAYS (Rate limited to ~10Hz)
+    static unsigned long save_timer = 0;
+    if (now - save_timer > 100)
     {
-        BluetoothSerial.print("DATA,");
-        BluetoothSerial.print(robotX);
-        BluetoothSerial.print(",");
-        BluetoothSerial.print(robotY);
-        BluetoothSerial.print(",");
-        BluetoothSerial.println(robot_heading * 180.0 / PI); // Deg for easier reading
-        print_timer = now;
+        // Only save if we haven't hit the memory limit
+        if (slam_point_count < MAX_SLAM_POINTS)
+        {
+            history_X[slam_point_count] = robotX;
+            history_Y[slam_point_count] = robotY;
+            slam_point_count++;
+        }
+        else
+        {
+            // Optional: Warn if array is full
+            // BluetoothSerial.println("WARNING: SLAM Data Array Full!");
+        }
+        save_timer = now;
     }
 }
 
-// #include "slam.h"
-// #include "helpers.h"
-// #include "sensors.h"
+void dump_slam_data()
+{
+    BluetoothSerial.println("--- BEGIN SLAM DATA DUMP ---");
+    BluetoothSerial.println("X, Y, Heading"); // CSV Header
 
-// // states for servo sweeping with the ultrasonic sensor
-// enum US_STATE
-// {
-//     POINT_FRONT,
-//     WAIT_FRONT,
-//     READ_FRONT,
-//     POINT_LEFT,
-//     WAIT_LEFT,
-//     READ_LEFT,
-//     POINT_BACK,
-//     WAIT_BACK,
-//     READ_BACK
-// };
+    for (int i = 0; i < slam_point_count; i++)
+    {
+        BluetoothSerial.print(history_X[i]);
+        BluetoothSerial.print(",");
+        BluetoothSerial.print(history_Y[i]);
+    }
 
-// US_STATE us_state = POINT_FRONT;
-// unsigned long us_timer = 0;
-// const int SERVO_DELAY = 300; // ms to allow servo to physically move
+    BluetoothSerial.println("--- END SLAM DATA DUMP ---");
 
-// // latest US readings
-// float latest_us_front = -1;
-// float latest_us_left = -1;
-// float latest_us_back = -1;
-
-// void init_slam()
-// {
-//     // start pointing forward, think thats 0 degree? need to check
-//     turret_motor.write(0);
-// }
-
-// // helper functions to set sensor limits
-// float getValidSR(float raw)
-// {
-//     if (raw >= 4.0 && raw <= 30.0)
-//     {
-//         return raw;
-//     }
-//     return -1.0;
-// }
-
-// float getValidLR(float raw)
-// {
-//     if (raw >= 10.0 && raw <= 80.0)
-//     {
-//         return raw;
-//     }
-//     return -1.0;
-// }
-
-// void budget_slam()
-// {
-//     unsigned long now = millis();
-
-//     // no blockinggggg
-//     switch (us_state)
-//     {
-//     case POINT_FRONT:
-//         turret_motor.write(0); // front
-//         us_timer = now;
-//         us_state = WAIT_FRONT;
-//         break;
-//     case WAIT_FRONT:
-//         if (now - us_timer >= SERVO_DELAY)
-//             us_state = READ_FRONT;
-//         break;
-//     case READ_FRONT:
-//         latest_us_front = getUSDistance();
-//         us_state = POINT_LEFT;
-//         break;
-
-//     case POINT_LEFT:
-//         turret_motor.write(90); // left
-//         us_timer = now;
-//         us_state = WAIT_LEFT;
-//         break;
-//     case WAIT_LEFT:
-//         if (now - us_timer >= SERVO_DELAY)
-//             us_state = READ_LEFT;
-//         break;
-//     case READ_LEFT:
-//         latest_us_left = getUSDistance();
-//         us_state = POINT_BACK;
-//         break;
-
-//     case POINT_BACK:
-//         turret_motor.write(180); // back
-//         us_timer = now;
-//         us_state = WAIT_BACK;
-//         break;
-//     case WAIT_BACK:
-//         if (now - us_timer >= SERVO_DELAY)
-//             us_state = READ_BACK;
-//         break;
-//     case READ_BACK:
-//         latest_us_back = getUSDistance();
-//         us_state = POINT_FRONT; // go back and start again, maybe change ? ------------------------------------
-//         break;
-//     }
-
-//     // reading sr sensors (front)
-//     float sr_left = getValidSR(getLeftSR());
-//     float sr_right = getValidSR(getRightSR());
-
-//     // reading lr sensors (right)
-//     float lr_front = getValidLR(getRightLR());
-//     float lr_back = getValidLR(getLeftLR());
-
-//     // for the helper addCoordEst
-//     int x_count = 0;
-//     int y_count = 0;
-//     float sum_x = 0;
-//     float sum_y = 0;
-
-//     // uses the angle of the robot (and servo) to work out the accurate dist values
-//     auto addCoordinateEstimate = [&](float distance, float mounted_angle)
-//     {
-//         if (distance < 0)
-//         {
-//             return; // ignores the out of range sensors
-//         }
-
-//         // this is the robot angle + the servo turned angle (mounted angle will be 0 for ir sensors)
-//         float global_angle = robot_heading + mounted_angle;
-
-//         // warp angle to stay within -PI and +PI
-//         while (global_angle > PI)
-//             global_angle -= TWO_PI;
-//         while (global_angle < -PI)
-//             global_angle += TWO_PI;
-
-//         // calc x axis, if cos(angle) is > 0.707 or < -0.707, it is pointing mostly forward/backward
-//         if (abs(cos(global_angle)) > 0.707)
-//         {
-//             if (cos(global_angle) > 0)
-//             {
-//                 // pointing front and taking away the dist from the total to work out where we are
-//                 // cos(angle) * hypotenuse = adjacent
-//                 sum_x += TABLE_HEIGHT - (distance * abs(cos(global_angle)));
-//             }
-//             else
-//             {
-
-//                 sum_x += distance * abs(cos(global_angle));
-//             }
-//             x_count++;
-//         }
-
-//         // calc y axis, if sin(angle) is > 0.707 or < -0.707, it is pointing mostly sideways
-//         if (abs(sin(global_angle)) > 0.707)
-//         {
-//             if (sin(global_angle) > 0)
-//             {
-//                 // sin(angle) * hypotenuse = y
-//                 sum_y += distance * abs(sin(global_angle));
-//             }
-//             else
-//             {
-//                 // the opposite taking away the table width to make all the same
-//                 sum_y += TABLE_WIDTH - (distance * abs(sin(global_angle)));
-//             }
-//             y_count++;
-//         }
-//     };
-
-//     // RADIANSSSSSSSSSSSSSS
-//     // front = 0, left = pi/2 (1.57), back = pi (3.14)
-
-//     // sr ir
-//     addCoordinateEstimate(sr_left, 0.0);
-//     addCoordinateEstimate(sr_right, 0.0);
-
-//     // lr ir
-//     addCoordinateEstimate(lr_front, -HALF_PI);
-//     addCoordinateEstimate(lr_back, -HALF_PI);
-
-//     // ultrasonic front left right
-//     addCoordinateEstimate(latest_us_front, 0.0);
-//     addCoordinateEstimate(latest_us_left, HALF_PI);
-//     addCoordinateEstimate(latest_us_back, PI);
-
-//     // 4. AVERAGE THE VALID ESTIMATES
-//     if (x_count > 0)
-//         robotX = sum_x / x_count;
-//     if (y_count > 0)
-//         robotY = sum_y / y_count;
-
-//     // 5. OUTPUT TO BLUETOOTH (Rate limited to ~10Hz to prevent buffer crashing)
-//     static unsigned long print_timer = 0;
-//     if (now - print_timer > 100)
-//     {
-//         BluetoothSerial.print("DATA,");
-//         BluetoothSerial.print(robotX);
-//         BluetoothSerial.print(",");
-//         BluetoothSerial.print(robotY);
-//         BluetoothSerial.print(",");
-//         BluetoothSerial.println(robot_heading); // Sending heading is highly useful for mapping!
-//         print_timer = now;
-//     }
-// }
-
-// // void budget_SLAM(float USdistances[], float LeftIRReadings[], float RightIRReadings[], float angles[], int count)
-// // {
-
-// //     // find closest wall
-// //     int closest_wall_index_rough = FindMinIndex(USdistances, count);
-
-// //     // check with irs first (in case we are in middle)
-// //     float checkIR = (LeftIRReadings[closest_wall_index_rough] + RightIRReadings[closest_wall_index_rough]) / 2.0;
-
-// //     // default to closest wall (US reading)
-// //     int closest_wall_index = closest_wall_index_rough;
-
-// //     if (checkIR > 5.0 && checkIR < 75.0)
-// //     {
-// //         float best_IR_sum = 999.0; // arbitrary num
-
-// //         for (int j = -5; j <= 5; j++)
-// //         {
-// //             int check_idx = (closest_wall_index_rough + j + count) % count;
-// //             // seeing the diff between front ir sensors
-// //             float current_IR_sum = LeftIRReadings[check_idx] + RightIRReadings[check_idx];
-
-// //             if (current_IR_sum < best_IR_sum)
-// //             {
-// //                 best_IR_sum = current_IR_sum;
-// //                 closest_wall_index = check_idx;
-// //             }
-// //         }
-// //     }
-// //     else
-// //     {
-
-// //         BluetoothSerial.println("you are in the fucking middle you lunatic");
-// //     }
-
-// //     // get the other 3 walls (90 degrees apart)
-// //     int wall_two_index = (closest_wall_index + (count / 4)) % count;
-// //     int wall_three_index = (closest_wall_index + (count / 2)) % count;
-// //     int wall_four_index = (closest_wall_index + (3 * count / 4)) % count;
-
-// //     // calculate the distances
-// //     float distN = USdistances[closest_wall_index];
-// //     float distW = USdistances[wall_two_index];
-// //     float distS = USdistances[wall_three_index];
-// //     float distE = USdistances[wall_four_index];
-
-// //     // calculate length and width of the table (should be 200 x 100?) robot size declared at top of code, need to update
-// //     float roomWidthCalc = distW + distE + JOHN_ROBOT_WIDTH;
-// //     float roomHeightCalc = distN + distS + JOHN_ROBOT_LENGTH;
-
-// //     // calculate the x and y coord based on the bottom left corner (south and west)
-// //     robotX = distW + (JOHN_ROBOT_WIDTH / 2.0);
-// //     robotY = distS + (JOHN_ROBOT_LENGTH / 2.0);
-
-// //     // --- Output Results to Bluetooth ---
-// //     BluetoothSerial.println("--- SLAM LOCALIZATION ---");
-// //     BluetoothSerial.print("DATA,");
-// //     BluetoothSerial.print(robotX);
-// //     BluetoothSerial.print(",");
-// //     BluetoothSerial.print(robotY);
-// //     BluetoothSerial.print(",");
-// //     BluetoothSerial.print(roomWidthCalc);
-// //     BluetoothSerial.print(",");
-// //     BluetoothSerial.println(roomHeightCalc);
-// // }
+    // Optional: Reset the count if you plan to run the robot again without resetting power
+    // slam_point_count = 0;
+}
